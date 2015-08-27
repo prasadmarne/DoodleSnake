858b289d7f05d0a185771a0fb3c68e3c474f8f77
prasadmarne <praddymarne@gmail.com>
2015-08-27 07:48:15 +0530

    Added project files

---
+++ src/com/praddy/androidgames/doodlesnake/SnakePart.java
@@ -0,0 +1,11 @@
+package com.praddy.androidgames.doodlesnake;
+
+public class SnakePart {
+    public int x, y;
+    
+    public SnakePart(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}       
+
